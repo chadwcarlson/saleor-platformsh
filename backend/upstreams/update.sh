0f679a354b50219d1cfd447c59370f2112416bed
chadcarlson <chad.carlson@platform.sh>
2021-06-03 14:25:07 -0400

    Updated poetry (1.1.5 -> 1.1.6).

--- backend/upstreams/update.sh
+++ backend/upstreams/update.sh
@@ -9,7 +9,7 @@ RELEASE_URL=https://api.github.com/repos/$NAMESPACE/releases/latest
 LATEST_VERSION=$(curl -s $RELEASE_URL| jq -r '.tag_name') 
 echo -e "\nRunning scheduled upstream update for \033[1m$UPSTREAM\033[0m\n"
 if [ "$CURRENT_VERSION" = "$LATEST_VERSION" ]; then
-    echo -e "- Current version (\033[1m$CURRENT_VERSION\033[0m) is still the latest.\n- Skipping update.\n"
+    echo -e "Current version (\033[1m$CURRENT_VERSION\033[0m) is still the latest.\nSkipping update.\n"
 else
     echo -e "An update is available for \033[1m$UPSTREAM\033[0m.\n\n    * \033[1mLatest:\033[0m  $LATEST_VERSION\n    * \033[1mCurrent:\033[0m $CURRENT_VERSION\n"
     python upstreams/bump.py $UPSTREAM $LATEST_VERSION
