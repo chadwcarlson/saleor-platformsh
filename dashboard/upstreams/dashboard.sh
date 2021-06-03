b936838570d7201de71c3d0acc4d7c5357b1f1e3
chadcarlson <chad.carlson@platform.sh>
2021-06-03 16:40:35 -0400

    add dash placeholder.

---
+++ dashboard/upstreams/dashboard.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+
+VERSIONS_FILE=upstreams/versions.json
+UPSTREAM=$(cat $VERSIONS_FILE | jq -r '.dashboard.upstream')
+VERSION=$(cat $VERSIONS_FILE | jq -r '.dashboard.version')
+echo -e "\nDownloading Saleor dashboard (\033[1m$VERSION\033[0m)\n"
+wget --quiet -c https://github.com/$UPSTREAM/archive/refs/tags/$VERSION.tar.gz -O - | tar -xz
+# Remove existing saleor subdirectory if on local.
+if [ -d saleor-dashboard ]; then
+    rm -rf saleor-dashboard
+fi
+mv saleor-dashboard-$VERSION saleor-dashboard
