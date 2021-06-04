ee73aaaa72de10542db50d1633c46aed7cf4f6d5
chadcarlson <chad.carlson@platform.sh>
2021-06-04 12:43:07 -0400

    clean up.

--- backend/scripts/_build.sh
+++ backend/scripts/_build.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+me=$(basename $BASH_SOURCE)
+./scripts/_aline.sh START $me
+
 # Get and configure Poetry.
 ./scripts/download_poetry.sh
 ./scripts/setup_poetry.sh
@@ -8,4 +11,4 @@
 ./scripts/download_saleor.sh
 ./scripts/setup_saleor.sh
 
-./scripts/_aline.sh
+./scripts/_aline.sh END $me
