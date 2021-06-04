9df11a76a7ad22b5a2eb19093f3e2257ac129045
chadcarlson <chad.carlson@platform.sh>
2021-06-04 16:08:11 -0400

    actually pass it.

--- backend/scripts/download_nvm.sh
+++ backend/scripts/download_nvm.sh
@@ -1,12 +1,13 @@
 #!/usr/bin/env bash
 
 # Check build cache usage https://gist.github.com/devicezero/b38ed48bccaef72a0ab24293552992d8
+VERSION=$1
 echo -e "\nDownloading NVM (\033[1m$VERSION\033[0m)\n"
 NODE_VERSION=$(cat .nvmrc)
 unset NPM_CONFIG_PREFIX
 export NVM_DIR="$PLATFORM_APP_DIR/.nvm"
 if [ ! -d "$NVM_DIR/" ]; then
     # install.sh will automatically install NodeJS based on the presence of $NODE_VERSION
-    curl -f -o- https://raw.githubusercontent.com/nvm-sh/nvm/$NVM_VERSION/install.sh | bash
+    curl -f -o- https://raw.githubusercontent.com/nvm-sh/nvm/$ERSION/install.sh | bash
 fi
 [ -s "$NVM_DIR/nvm.sh" ] && \. "$NVM_DIR/nvm.sh"
