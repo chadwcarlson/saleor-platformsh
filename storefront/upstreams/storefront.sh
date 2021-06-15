ca9620e1a98239a8c86e368cadd35230a4b33934
chadcarlson <chad.carlson@platform.sh>
2021-06-15 13:36:16 -0400

    commit directly to test.

--- storefront/upstreams/storefront.sh
+++ storefront/upstreams/storefront.sh
@@ -1,16 +1,16 @@
 #!/usr/bin/env bash
 
 set -e
-VERSIONS_FILE=upstreams/versions.json
-UPSTREAM=$(cat $VERSIONS_FILE | jq -r '.storefront.upstream')
-VERSION=$(cat $VERSIONS_FILE | jq -r '.storefront.version')
-echo -e "\nDownloading Saleor storefront (\033[1m$VERSION\033[0m)\n"
-wget --quiet -c https://github.com/$UPSTREAM/archive/refs/tags/$VERSION.tar.gz -O - | tar -xz
-# Remove existing saleor subdirectory if on local.
-if [ -d saleor-storefront ]; then
-    rm -rf saleor-storefront
-fi
-mv saleor-storefront-$VERSION saleor-storefront
+# VERSIONS_FILE=upstreams/versions.json
+# UPSTREAM=$(cat $VERSIONS_FILE | jq -r '.storefront.upstream')
+# VERSION=$(cat $VERSIONS_FILE | jq -r '.storefront.version')
+# echo -e "\nDownloading Saleor storefront (\033[1m$VERSION\033[0m)\n"
+# wget --quiet -c https://github.com/$UPSTREAM/archive/refs/tags/$VERSION.tar.gz -O - | tar -xz
+# # Remove existing saleor subdirectory if on local.
+# if [ -d saleor-storefront ]; then
+#     rm -rf saleor-storefront
+# fi
+# mv saleor-storefront-$VERSION saleor-storefront
 
 cd saleor-storefront
 npm install
